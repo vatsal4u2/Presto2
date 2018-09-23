6e0bd548d219709723b4f87fb3812dd959a45f98
Vatsal Patel <pvatsal23@gmail.com>
2018-09-23 03:26:48 -0400

    Rotate array by given number & Evaluate reverse polish notation exmaple

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
 public class Main {
 
     private static final String socksString = "1/blue/right,2/blue/right,3/red/right,4/blue/left,5/purple/left,6/red/left,7/green/left,8/red/left,9/blue/left";
-    static HashMap<String, String> map;
-    private static String[] commaSeparated;
-
+    private static HashMap<String, String> map;
+    static int[] array = {1,2,3,4,5,6,7};
+    static int byNumber = 3;
     public static void main(String[] args) {
         map = new HashMap<>();
-        commaSeparated = socksString.split(",");
-
+        String[] commaSeparated = socksString.split(",");
         for (String aCommaSeparated : commaSeparated) {
             String[] tem = aCommaSeparated.split("/");
             map.put(tem[0], tem[1] + "-" + tem[2]);
@@ -32,6 +33,76 @@ public class Main {
                 }
             }
         }
+
+       Main m = new Main();
+       int[] rotatedArray = m.RotateArrayByNumber(array,byNumber);
+       System.out.println(Arrays.toString(rotatedArray));
+
+       String[] tokens = new String[] { "2", "1", "+", "3", "*" };
+       int answer = m.evaluateRPN(tokens);
+       System.out.println(answer);
     }
 
+
+    /**
+     * These method is used to rotate the array by given number. These method is called bubble rotate method
+     * @param array
+     * @param byNumber
+     * @return
+     */
+
+    private int[] RotateArrayByNumber(int[] array, int byNumber){
+        if(array == null || byNumber < 0){
+            throw new IllegalArgumentException("Illegal argument..!");
+        }
+        for(int i = 0; i < byNumber; i++){
+            for(int j = array.length -1; j > 0; j--){
+                int tem = array[j];
+                array[j] = array[j-1];
+                array[j-1] = tem;
+            }
+        }
+        return array;
+    }
+
+    /**
+     *Evaluate reverse polish notation.
+     * @param token
+     * @return
+     */
+
+    private int evaluateRPN(String[] token){
+        int returnValue = 0;
+        String operator = "+-*/";
+
+        Stack<String> stack = new Stack<>();
+
+        for (String t:token){
+            if(!operator.contains(t)){
+                stack.push(t);
+            }else{
+                int a = Integer.valueOf(stack.pop());
+                int b = Integer.valueOf(stack.pop());
+                int index = operator.indexOf(t);
+                switch(index) {
+                    case 0:
+                        stack.push(String.valueOf(a + b));
+                        break;
+                    case 1:
+                        stack.push(String.valueOf(b - a));
+                        break;
+                    case 2:
+                        stack.push(String.valueOf(a * b));
+                        break;
+                    case 3:
+                        stack.push(String.valueOf(b / a));
+                        break;
+                }
+            }
+        }
+
+        returnValue = Integer.valueOf(stack.pop());
+
+        return returnValue;
+    }
 }
